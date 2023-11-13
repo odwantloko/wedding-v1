9e99244a47e6cef4c5ebe89e02543caa8eee456b
Odwa Ntloko <ntlokoo@outlook.com>
2023-11-13 19:11:47 +0200

    accom options

--- src/components/accomodation.jsx
+++ src/components/accomodation.jsx
@@ -7,20 +7,28 @@ export const Accomodation = (props) => {
         <div className="section-title">
           <h2>Accommodation</h2>
             <p>Rustenburg offers a wide range of charming guesthouses and accommodation choices that cater to various budgets and personal preferences. 
-              To assist you in your search, we've curated a selection of suggestions below. 
-              The options are ordered by nearest distance to the venues. Additionally, you also have the option to explore Airbnb listings.</p>
+              To assist you in your search, we've added selection of suggestions to start with below. 
+              The options are ordered by nearest distance to the venue. Additionally, you also have the option to explore Airbnb listings.</p>
         </div>
         
           <div className="story-items">
+            <div>
+              <p>                  
+                *Disclaimer: The room you end up actually staying in might differ from the one in the picture. To be safe, confirm when booking. 
+              </p>
+            </div>
             {props.data
               ? props.data.map((d, i) => (
                 <div className="col-sm-6 col-md-3 col-lg-3">
                 <h3><a href={d.link}>{d.title}</a></h3>
                   <img src={d.smallImage} className="img-responsive" alt={d.title} />{" "}
-                  <p>
+                  <p> Google Map link: <br/>
                     <a href={d.map}> 
                       {d.distance}
                     </a>
+                  </p>
+                  <p>
+                    Contact no: {d.contact}
                   </p>
                   </div>
                 ))
--- src/components/gettingThere.jsx
+++ src/components/gettingThere.jsx
@@ -7,13 +7,30 @@ export const GettingThere = (props) => {
         <div className="section-title">
           <h2>Getting There</h2>
           <p>
-            For directions on how to get there, you can use this Google Maps link: 
+            First of all, thank you all so much for your afforts in attending. It really means the world to us. We appreciate your love and support for our special day. <br/><br/>
+            For the exact location of the venue, you can use this Google Maps link: 
             <br/>
             <a href="https://www.google.com/maps/place/Hedgehog's+Nest/@-25.648849,27.162285,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipN_QbpvUwDW0wRiKh2A4dHDDxUmrGeCqQPnro07!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipN_QbpvUwDW0wRiKh2A4dHDDxUmrGeCqQPnro07%3Dw128-h86-k-no!7i3310!8i2207!4m7!3m6!1s0x1ebe0bec95843afb:0xb6bcbb52b8c86fff!8m2!3d-25.648849!4d27.162285!10e5!16s%2Fg%2F1tfpm2p6?entry=ttu">
             Hedgehog's nest 
             </a>
           </p>
-
+          <br/>
+          <p> 
+            For those of you travelling from Cape Town, you have 2 main options.
+          </p>
+          <h3>
+            Option 1: Road Trip!!
+          </h3>
+          <p>
+            Driving from Cape Town to Rustenburg, it is about a 15 hour drive. It is recommended that you split the drive over 2 days with a sleepover stop in Kimberley. This is a 10 hour drive. From Kimberley, you will be 5 hours away from Rustenburg.
+          </p>
+          <h3>
+            Option 2: The savvy timesaver
+          </h3>
+          <p>
+            You could fly directly from Cape Town Internation Airport to Lanseria International airport in Pretoria and hire a car for your time in Rustenburg.
+            The fastest route from Lanseria airport to Rustenburg is 1hr 22min. Flying to OR Tambo international airport is also an option but adds about 30-40 min to your journey.
+          </p>
           <p> </p>
         </div>
       </div>
