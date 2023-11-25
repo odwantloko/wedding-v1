666b513caa97cdfdd2d87ce21dfd8be618fb7f93
Odwa Ntloko <ntlokoo@outlook.com>
2023-11-25 14:06:06 +0200

    changed wording a bit

--- src/components/story.jsx
+++ src/components/story.jsx
@@ -12,7 +12,7 @@ export const Story = (props) => {
         <div className="section-title">
           <h2>Our Love Story</h2>  
           <div className="story-items">
-            <p>Over the years, we've been blessed with some wonderful moments. With too many to share, we've captured some of the highlights that helped defined our journey, inviting you to relive the magic that brought us to this joyous celebration.</p>
+            <p>Over the years, we've been blessed with some wonderful moments. With too many to share, we've captured some of our cherished highlights that helped defined our journey, inviting you to relive the magic that brought us to this point.</p>
           {props.data
             ? 
             <>
