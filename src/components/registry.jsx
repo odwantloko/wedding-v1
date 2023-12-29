3fc2e98c58c50b785aa31dd9ca346fb5fd7a6146
Nelson.Kadama <sskadama@gmail.com>
2023-12-29 22:02:49 +0200

    adding homeetc registry

--- src/components/registry.jsx
+++ src/components/registry.jsx
@@ -8,14 +8,14 @@ export const Registry = (props) => {
           <h2>Registry</h2>
           <div className="registry-content">
               <p>
-                If you'd like to help us start this new chapter in our lives with a little something extra, we have added a link to our gift registry below. We will also be accepting gift vouchers on the day.
+                If you'd like to help us start this new chapter in our lives with a little something extra, we have added links to our gift registries below. We will also be accepting gift vouchers on the day.
               </p>
               <br/>
               <h3>
                 How does the registry work?
               </h3>
               <p>
-                When you buy something from the registy, you purchase it online (through the link) but it gets <b>delivered to our home</b> on our wedding day. 
+                When you buy something from a registy, you purchase it online (through the link) but it gets <b>delivered to our home</b> on our wedding day.
                 This means that you don't have to buy a physical gift and carry it with you. There is also an option to add a gift voucher if you'd like to instead.
               </p>
         
